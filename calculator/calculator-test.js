bb4cf853b4939582dbb21d1003786196e0be44e3
Rohan Dutta <duttarohan4202@gmail.com>
2020-12-04 20:39:19 -0600

    files to add into calc and tips

---
+++ calculator/calculator-test.js
@@ -0,0 +1,12 @@
+
+it('should calculate the monthly rate correctly', function () {
+  const values = { amount: 10000, years: 10, rate: 4.5, };
+  expect(calculateMonthlyPayment(values)).toEqual('103.64')
+});
+
+
+it("should return a result with 2 decimal places", function() {
+  const values = { amount: 10043, years: 8, rate: 5.8 };
+  expect(calculateMonthlyPayment(values)).toEqual('131.00');
+});
+
